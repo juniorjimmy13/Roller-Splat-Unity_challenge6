fe319c4193d221b5d0bdb98201c8145ee986bd63
Juniorjimmy13 <juniorjimmy13@gmail.com>
2024-09-21 16:15:20 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+
+    [SerializeField]
+    private GroundPiece[] allGroundPieces;
+
+    private BallController player;
+
+    private void Start()
+    {
+        SetupNewLevel();
+    }
+
+    private void SetupNewLevel()
+    {
+        allGroundPieces = FindObjectsOfType<GroundPiece>();
+    }
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+      
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+        player = null;
+        
+
+
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<BallController>();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+        {
+            player.playExplosion();
+            StartCoroutine(NextLevel());
+            
+        }
+    }
+
+    private IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
